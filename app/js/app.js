5b5eeb4b94c0242ef3a56cf539f63564f68254f7
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-02-12 15:24:51 +0200

    Added filter and User list

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,7 @@
 	angular.module('app', ['ngMaterial', 'ngMdIcons'])
 				 .config(($mdIconProvider, $mdThemingProvider) => {
 				  	$mdIconProvider
+				  		.defaultIconSet('img/icons/avatars.svg', 128)
 				  		.icon('menu', 'img/icons/menu.svg', 24);
 
 				  	$mdThemingProvider
--- app/js/ctrl/mainCtrl.js
+++ app/js/ctrl/mainCtrl.js
@@ -2,12 +2,26 @@
 
 	'use strict';
 
-	let mainCtrl = function($mdSidenav) {
+	let mainCtrl = function($mdSidenav, usersFactory) {
 		let vm = this;
+		vm.selected = null;
+		vm.searchText = '';
 
 		vm.toggleSidenav = () => {
 			$mdSidenav('left').toggle();
 		}
+
+		vm.selectUser = (user) => {
+			vm.selected = user;
+
+			let sidenav = $mdSidenav('left');
+			if (sidenav.isOpen()) {
+				sidenav.close();
+			}
+		}
+
+		vm.users = usersFactory.users;
+		vm.selected = vm.users[0];
 
 	}
 
